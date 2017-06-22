b33ad6b3848028e74be6d8a79e1c41f9c4c8fcfd
William Huang <huang.william.cn@hotmail.com>
2017-06-22 15:57:32 +0800

    Complete post menu stucture. Add access token errors.

--- test/get_access_token_test.js
+++ test/get_access_token_test.js
@@ -2,7 +2,7 @@ var getToken = require('../lib/get_access_token');
 
 exports.queue_test = function (test) {
     test.expect(1);
-    getToken.getAccessToken((token) => {
+    getToken.getAccessToken((err,token) => {
         test.ok(token,'Should receive token.');
         test.done();
     });
--- test/post_menu_test.js
+++ test/post_menu_test.js
@@ -1,12 +1,20 @@
 var pm = require('../lib/post_menu');
 var getToken = require('../lib/get_access_token');
 
-exports.queue_test = function (test) {
+exports.postMenuSuccess_test = function (test) {
     test.expect(1);
-    getToken.getAccessToken((token) => {
-        pm.postMenu(token,(data) => {
-            test.ok(data,'Should receive data.');
+    getToken.getAccessToken((err, token) => {
+        pm.postMenu(token,(err) => {
+            test.doesNotThrow(err,Error,'Should not have error from WeChat server.');
             test.done();
         });
+    });
+};
+
+exports.postMenuFail_cridential_test = function (test) {
+    test.expect(1);
+    pm.postMenu('asdf',(err) => {
+        test.throws(() => {throw err},/invalid/,'Should receive error from WeChat server.');
+        test.done();
     });
 };
