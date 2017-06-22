3af93ebacaf2dedd959ab90acabef240cbde50e0
William Huang <huang.william.cn@hotmail.com>
2017-06-22 15:21:03 +0800

    Construct post_menu.

--- test/post_menu_test.js
+++ test/post_menu_test.js
@@ -1,9 +1,12 @@
+var pm = require('../lib/post_menu');
 var getToken = require('../lib/get_access_token');
 
 exports.queue_test = function (test) {
     test.expect(1);
     getToken.getAccessToken((token) => {
-        test.ok(token,'Should receive token.');
-        test.done();
+        pm.postMenu(token,(data) => {
+            test.ok(data,'Should receive data.');
+            test.done();
+        });
     });
 };
