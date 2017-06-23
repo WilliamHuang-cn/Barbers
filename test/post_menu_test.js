da259a4a8b10a910ab58e6f4e8fbf45d683051d5
William Huang <huang.william.cn@hotmail.com>
2017-06-23 21:21:36 +0800

    Fix bug: change postMenu HTTP method to POST. Complete intergration of get_access_token.js and post_menu.js.

--- test/post_menu_test.js
+++ test/post_menu_test.js
@@ -4,7 +4,7 @@ exports.postMenuSuccess_test = function (test) {
     test.expect(1);
     weChat.getAccessToken((err, token) => {
         weChat.postMenu(token,(err) => {
-            test.doesNotThrow(err,Error,'Should not have error from WeChat server.');
+            test.equal(err,null,'Should not have error from WeChat server.');
             test.done();
         });
     });
