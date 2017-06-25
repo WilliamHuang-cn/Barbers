9825a4b5d7ab65442303dd6fea4b9b50773b40f7
William Huang <huang.william.cn@hotmail.com>
2017-06-25 11:50:11 +0800

    Seperating customer menu and barber menu. Start using connect to deal with web page responding.

--- barbers_server.js
+++ barbers_server.js
@@ -1,9 +1,10 @@
 var http = require('http');
 var qs = require('querystring');
 var api = require('./lib/weChatAPI');
+var connect = require('connect');
 
 api.getAccessToken((err,token) => {
-    api.postMenu(token,'./public/menu.json',(err) => {
+    api.postMenu(token,'./public/CustomMenu.json',(err) => {
     if (err != null) {
         console.log('Error posting menu: '+err);
     }});
@@ -21,25 +22,58 @@ var server = http.createServer((req,res) => {
       var parseString = require("xml2js").parseString;
       parseString(body, function (err, data) {
         if(!err){
-          console.log(data)
-          switch (data.xml.MsgType[0]) {
+          console.log(data);
+          if (data == null || data.xml == undefined) {
+              return;
+          }
+          data = data.xml;              // Remove xml layer
+          switch (data.MsgType[0]) {
               case 'event':
-                  console.log('An event is received;');
-
+                //   console.log('An event is received;');
+                  switch (data.Event[0]) {
+                      case 'CLICK':
+                          clickEventHandler(data,res);
+                          break;
+                      default:
+                      res.end('success');
+                  }
                   break;
               case 'text':
                   console.log('A text is received.');
+                  res.end('success');
                   break;
               default:
                   console.log('Unknown type received: '+data.xml.MsgType[0]);
+                  res.end('success');
           }
-
-          res.end('success');
         }
       });
     });
 });
 
+server.use((req,res,next) => {
+    
+});
+
 server.listen(80, () => {
     console.log('Server running on port 80.');
 });
+
+function clickEventHandler(data,res) {
+    switch (data.EventKey[0]) {
+        case 'V1001_QUEUE_QUERY':
+            const customerCount = 3;
+            const waitingTime = 5
+            const reply = '在您之前有'+customerCount+'人，预计等待时间约'+waitingTime+'分钟';
+            res.end('<xml>'+
+            '<ToUserName><![CDATA['+data.FromUserName+']]></ToUserName>'+
+            '<FromUserName><![CDATA['+data.ToUserName+']]></FromUserName>'+
+            '<CreateTime>1348831860</CreateTime>'+
+            '<MsgType><![CDATA[text]]></MsgType>'+
+            '<Content><![CDATA['+reply+']]></Content>'+
+            '</xml>');
+            break;
+        default:
+            res.end('success');
+    }
+}
