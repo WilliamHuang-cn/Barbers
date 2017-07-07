5fab70f51f5b32b6ed82dde0f2ecea63c08d5728
William Huang <huang.william.cn@hotmail.com>
2017-07-07 21:41:41 +0800

    Construct remove customer function in queue module.

--- test/barber_queue_test.js
+++ test/barber_queue_test.js
@@ -93,8 +93,8 @@ exports.queue_add_repeat_id_fail_test = function (test) {
     });
 }
 
-exports.queue_remove_success_test = function (test) {
-    test.expect(6);
+exports.queue_pop_success_test = function (test) {
+    test.expect(7);
     const customer1 = {
         'openid':'adsfasfaecqwefqecqwedsf-2',
         'serviceType':'wash',
@@ -112,9 +112,7 @@ exports.queue_remove_success_test = function (test) {
     };
 
     queue.addCustomerToQueue(customer1,(err) => {});
-    queue.addCustomerToQueue(customer2,(err) => {
-        // console.log(err);
-});
+    queue.addCustomerToQueue(customer2,(err) => {});
     queue.addCustomerToQueue(customer3,(err) => {});
 
     queue.totalEstimatedTime((err,time) => {
@@ -150,5 +148,47 @@ exports.queue_remove_fail_test = function (test) {
     queue.nextCustomerFromQueue((err,nextCust) => {
         test.throws(()=>{throw err},'Queue should throw upon queue depletion.');
         test.done();
+    });
+}
+
+exports.queue_remove_success_test = function (test) {
+    test.expect(5);
+    const customer1 = {
+        'openid':'adsfasfaecqwefqecqwedsf-2',
+        'serviceType':'wash',
+    };
+    const customer2 = {
+        'openid':'asd-23rdf3',
+        'serviceType':'wash',
+    };
+    const customer3 = {
+        'openid':'adsfasfaecdwedsf-23rdf3',
+        'serviceType':'perm',
+    };
+
+    queue.addCustomerToQueue(customer1,(err) => {});
+    queue.addCustomerToQueue(customer2,(err) => {});
+    queue.addCustomerToQueue(customer3,(err) => {});
+
+    var count = 3;
+    queue.nextCustomerFromQueue((err,nextCust) => {
+        test.equal(err,null,'Queue should not throw error upon removing customer.');
+        test.equal(customer1,nextCust,'Queue should remove customer 1.');
+        if (--count == 0) {
+            test.done();
+        }
+    });
+    queue.removeCustomerFromQueue({'openid':'adsfasfaecdwedsf-23rdf3'},(err) => {
+        test.equal(err,null,'Queue should not throw error upon removing customer.');
+        if (--count == 0) {
+            test.done();
+        }
+    });
+    queue.nextCustomerFromQueue((err,nextCust) => {
+        test.equal(err,null,'Queue should not throw error upon removing customer.');
+        test.equal(customer2,nextCust,'Queue should remove customer 2.');
+        if (--count == 0) {
+            test.done();
+        }
     });
 }
