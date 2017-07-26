670462e043734392c70e56101d1ab11c7de43a48
William Huang <huang.william.cn@hotmail.com>
2017-07-26 15:10:57 +0800

    Successful form result return.

--- public/javascripts/register_ui.js
+++ public/javascripts/register_ui.js
@@ -6,9 +6,16 @@ $(document).ready(function () {
 
     // $("#testid").val("hello");
     $("#submit").on('click',function(){
-        var name = $("#name").val;
-        var tel = $("#telnumber").val;
-        var sex = $("#sex").val;
-        socket.emit('joinQueue',{name:name,sex:sex,tel:tel});
+        var name = $("#name").val();
+        var tel = $("#telnumber").val();
+        var sex = $("#sex").val();
+        var service = $("#service_type").val();
+        var remark = $("#remarks").val();
+        if (name != '' && tel != '') {
+            socket.emit('joinQueue',{name:name,sex:sex,tel:tel,seriveType:service,remark:remark});
+        }
+        else {
+            alert('Incomplete info!');
+        }
     });
 });
