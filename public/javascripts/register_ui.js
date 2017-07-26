abbdda5b00fdff2ba7308134723566765baf71d1
William Huang <huang.william.cn@hotmail.com>
2017-07-26 10:52:24 +0800

    socket.io tweek no.2

--- public/javascripts/register_ui.js
+++ public/javascripts/register_ui.js
@@ -1,8 +1,10 @@
-// const io = require('socket.io-client');
+const io = require('socket.io-client');
 var socket = io();
 
 $(document).ready(function {
     socket.emit('hello');
+
+    $("#testid").val = 'hello';
     // $("#submit").on('click',function(){
     //     socket.emit('joinQueue',{});
     // });
